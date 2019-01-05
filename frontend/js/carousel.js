0d4ff8866a6b95b743b20410ef4f4164676024ee
Tolik Dem <tolik.demianov@gmail.com>
2019-01-05 12:12:55 +0300

    edit carousel

--- frontend/js/carousel.js
+++ frontend/js/carousel.js
@@ -1,24 +1,36 @@
-    /* конфигурация */
-    var width = 309; // ширина изображения/карточки
-    var count = 2 // количество изображений/карточек для сдвига
+(function () {
+  /* конфигурация */
+  var width = 30.9; // ширина изображения/карточки
+  var count = 2 // количество изображений/карточек для сдвига
+  var offset = 16.0 + 3.6; // отступ
 
-    var carousel = document.getElementById('carousel');
-    var list = carousel.querySelector('.carousel ul');
-    var listElems = carousel.querySelectorAll('.carousel li');
+  var carousel = document.getElementById('carousel');
+  var list = carousel.querySelector('.carousel ul');
+  var listElems = carousel.querySelectorAll('.carousel li');
 
+  var position = 0; // текущий сдвиг влево
 
-    var position = 0; // текущий сдвиг влево
+  carousel.querySelector('.carousel__btn--left').onclick = function () {
+    // сдвиг влево
+    // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+    position = Math.min(position + width * count, offset);
+    list.style.marginLeft = position + 'rem';
+  };
 
-    carousel.querySelector('.carousel__btn--left').onclick = function () {
-      // сдвиг влево
-      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
-      position = Math.min(position + width * count, 160 + 36);
-      list.style.marginLeft = position + 'px';
-    };
+  carousel.querySelector('.carousel__btn--right').onclick = function () {
+    // сдвиг вправо
+    // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+    position = Math.max(position - width * count, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'rem';
+  };
 
-    carousel.querySelector('.carousel__btn--right').onclick = function () {
-      // сдвиг вправо
-      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
-      position = Math.max(position - width * count, -width * (listElems.length - count));
-      list.style.marginLeft = position + 'px';
-    };
+  function changeParametres() {
+    var screen = window.innerWidth;
+    if (screen < 600) {
+      count = 1;
+    }
+  }
+
+  window.addEventListener('resize', changeParametres);
+  document.addEventListener('DOMContentLoaded', changeParametres);
+})();
